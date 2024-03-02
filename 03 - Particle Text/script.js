65fec26e51f127a7dd026cab249bef6046e95135
Pradhan-G <abhishekpradhan12.10.00@gmail.com>
2024-03-03 00:50:09 +0530

    chapter 4 finished https://www.youtube.com/watch?v=2F2t1RJoGt8
    will do chapter 5 next

--- 03 - Particle Text/script.js
+++ 03 - Particle Text/script.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function () {
 
   drawLines(canvas.width / 2, canvas.height / 2);
 
-  const text = "Hello!";
+  //   const text = "Hello";
   const textX = canvas.width / 2;
   const textY = canvas.height / 2;
 
@@ -17,15 +17,41 @@ window.addEventListener("load", function () {
   context.strokeStyle = "blue";
   context.lineWidth = 3;
 
-  context.font = "180px Arial";
+  context.font = "80px Arial";
   context.textAlign = "center";
   context.textBaseline = "middle";
-  context.fillText(text, textX, textY);
-  context.strokeText(text, textX, textY);
+  //   context.fillText(text, textX, textY);
+  //   context.strokeText(text, textX, textY);
+
+  const maxTextWidth = canvas.width * 1;
+  // Text Wrap function
+  function wrapText(text) {
+    let linesArray = [];
+    let lineCounter = 0;
+    let line = "";
+    let words = text.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      let testline = line + words[i] + " ";
+      console.log(context.measureText(testline).width);
+      if (context.measureText(testline).width > maxTextWidth) {
+        line = words[i] + " ";
+        lineCounter++;
+      } else {
+        line = testline;
+      }
+      linesArray[lineCounter] = line;
+      // context.fillText(testline, canvas.width / 2, canvas.height / 2 + i * 80);
+    }
+    linesArray.forEach((line, index) => {
+      context.fillText(line, canvas.width / 2, canvas.height / 2 + index * 70);
+      lineCounter++;
+    });
+  }
+
+  wrapText("Hey how are you whats good today hope you are doing well");
 });
 
 // creating a function to draw two lines that goes through x and y axis in the centre of the canvas
-
 function drawLines(x, y) {
   //  Drawing the x axis
   context.lineWidth = 3;
